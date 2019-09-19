ef19e3978a68cc766ba50eaf2726fb665a534a84
pravesh172 <55476630+pravesh172@users.noreply.github.com>
2019-09-19 11:42:39 +0530

    new

---
+++ stopwatch.py
@@ -0,0 +1,10 @@
+# Python program to create a 
+# a new window using Tkinter 
+# importing the required libraires 
+import tkinter 
+  
+# creating a object 'top' as instance of class Tk 
+top = tkinter.Tk() 
+  
+# This will start the blank window 
+top.mainloop() 
